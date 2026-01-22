6483abeaf0df6ef9d41b943e7214d5f881389c42
Chris Ababan <chris@ababan.us>
2026-01-21 22:19:24 -0500

    feat: add BackButton component

--- src/components/back-button.stories.ts
+++ src/components/back-button.stories.ts
@@ -10,3 +10,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,3 +1,28 @@
-const BackButton = () => <h1>TEST</h1>;
+import { twMerge } from "tailwind-merge";
+import arrowLeft from "../images/arrow-left.svg";
+
+interface BackButtonProps {
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const BackButton = ({ disabled = false, onClick  }: BackButtonProps) => {
+  const className = twMerge(
+    'w-12 h-14 rounded-lg flex items-center justify-center transition-colors',
+    disabled
+      ? 'bg-red-300 cursor-not-allowed'
+      : 'bg-red-700 hover:bg-red-800 cursor-pointer'
+  );
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={className}
+    >
+      <img src={arrowLeft} alt="Back" width="28" height="24" />
+    </button>
+  );
+}
 
 export default BackButton;
