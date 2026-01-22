08aa26a67955ef9afd968a1dd533c61240c5bc88
Chris Ababan <chris@ababan.us>
2026-01-21 22:56:23 -0500

    chore: add react-scan to see perf issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { scan } from "react-scan";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+if (typeof window !== "undefined") {
+  scan({
+    enabled: true,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
