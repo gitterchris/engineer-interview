fa293cc022764c9ea5c9c7cc3cccdf2e89526c72
Chris Ababan <chris@ababan.us>
2026-01-21 22:54:36 -0500

    feat: add AddTaskField component

--- src/page/add-task-field.tsx
+++ src/page/add-task-field.tsx
@@ -1,3 +1,22 @@
-const AddTaskField = () => null;
+import { useAtom } from 'jotai';
+import { currentTaskAtom } from '../store/atom';
+
+const AddTaskField = () => {
+  const [currentTask, setCurrentTask] = useAtom(currentTaskAtom);
+
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentTask(e.target.value);
+  }
+
+  return (
+    <input
+      type="text"
+      value={currentTask}
+      onChange={handleTaskChange}
+      placeholder="Add Task"
+      className="w-full p-4 text-lg border border-black focus:outline-none"
+    />
+  );
+}
 
 export default AddTaskField;
